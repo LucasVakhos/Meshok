8774a12eb6c00dcd9b7091553252f5489f99bff5
Lukas Vakhos <lukasvakhos@gmail.com>
2026-07-14 02:54:22 +0300

    Add namespace compatibility for migrated GH components

---
+++ GH.Components/Compatibility/LegacyNamespaces.cs
@@ -0,0 +1,12 @@
+// Compatibility namespaces for legacy using directives.
+namespace GH.AppContext { public sealed class NamespaceMarker { } }
+namespace GH.Attributes { public sealed class NamespaceMarker { } }
+namespace GH.Configs { public sealed class NamespaceMarker { } }
+namespace GH.Controls { public sealed class NamespaceMarker { } }
+namespace GH.Grids { public sealed class NamespaceMarker { } }
+namespace GH.Helpers { public sealed class NamespaceMarker { } }
+namespace GH.Interfaces { public sealed class NamespaceMarker { } }
+namespace GH.NHibernate { public sealed class NamespaceMarker { } }
+namespace GH.UserForms { public sealed class NamespaceMarker { } }
+namespace GH.Utils { public sealed class NamespaceMarker { } }
+namespace GH.Components.Controls { public sealed class NamespaceMarker { } }
